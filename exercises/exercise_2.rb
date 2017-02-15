b82ff341f4b014bb305612f3b61f92125c87979e
Phelan Lewis <phelanlewis@gmail.com>
2017-02-15 01:46:20 +0000

    exercise 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,7 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+Store.instance_variable_set(:@store1, {'id' => '1'})
+Store.instance_variable_set(:@store2, {'id' => '2'})
+store = Store.find_by(id: '1')
+store.update(name: 'Vancouver')
