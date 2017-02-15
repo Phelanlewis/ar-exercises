f5116024a13be659f2885ea42463033d574edad2
Phelan Lewis <phelanlewis@gmail.com>
2017-02-15 01:49:20 +0000

    exercise 3 done

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+Store.instance_variable_set(:@store3, {'id' => '3'})
+store = Store.find_by(id: 3)
+store.destroy
+
+puts Store.count
